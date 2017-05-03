5e736c119a9a4654154b3d07e14179b7639725d4
Doug DeJulio <ddj@cmu.edu>
2017-05-03 10:17:52 -0400

    Just used "mvn" to create the project.

---
+++ src/main/java/org/aisb/karaf-commands/whoami.java
@@ -0,0 +1,27 @@
+
+package org.aisb.karaf-commands;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Option;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+
+@Command(scope = "aisb", name = "whoami", description = "List currently active principals according to JAAs.")
+@Service
+public class whoami implements Action {
+
+    @Option(name = "-o", aliases = { "--option" }, description = "An option to the command", required = false, multiValued = false)
+    private String option;
+
+    @Argument(name = "argument", description = "Argument to the command", required = false, multiValued = false)
+    private String argument;
+
+    @Override
+    public Object execute() throws Exception {
+         System.out.println("Executing command whoami");
+         System.out.println("Option: " + option);
+         System.out.println("Argument: " + argument);
+         return null;
+    }
+}
